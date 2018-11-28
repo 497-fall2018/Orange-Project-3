8bc631215733ba04d811bb3a885ec2dcfd54f6b0
Sweisian <rswei888@gmail.com>
2018-11-27 22:07:40 -0600

    added css

--- roo/screens/Main/Main.js
+++ roo/screens/Main/Main.js
@@ -5,16 +5,27 @@ import { Container, Header, Content, Button, Text } from 'native-base';
 export class Main extends React.Component {
   render() {
     return (
-      <View styles={styles.container}>
-        <Button block
-          onPress={() => this.props.navigation.navigate('Host')}>
-          <Text>Host</Text>
-        </Button>
+      <View style={styles.container}>
 
-        <Button block 
-          onPress={() => this.props.navigation.navigate('Join')}>
-          <Text>Join</Text>
-        </Button>
+          <Text style={styles.title}>
+            Roo
+          </Text>
+
+          <Text style={styles.description}>
+            Meetings made simple.
+          </Text>
+
+          <Button block
+            style={styles.button}
+            onPress={() => this.props.navigation.navigate('Host')}>
+            <Text>Host</Text>
+          </Button>
+
+          <Button block
+            style={styles.button}
+            onPress={() => this.props.navigation.navigate('Join')}>
+            <Text>Join</Text>
+          </Button>
 
       </View>
     );
@@ -23,10 +34,33 @@ export class Main extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    //display: 'flex',
     flex: 1,
     justifyContent: 'center',
     alignContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
+    paddingBottom: 100,
+  },
+
+  button: {
+    marginTop: 10,
+    marginBottom: 10,
+    marginRight: 20,
+    marginLeft: 20,
+  },
+
+  title: {
+    marginTop: 10,
+    //marginBottom: 10,
+    fontSize: 120
+    //fontFamily: "Roboto"
+  },
+
+  description: {
+    marginTop: 5,
+    marginBottom: 40,
+    //fontFamily: "Roboto",
+    fontStyle: 'italic'
   },
 });
